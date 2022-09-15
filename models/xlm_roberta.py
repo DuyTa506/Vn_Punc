b3aba99188735a5afa04c5ec54b118d4cc1dadfa
duyfaker04@gmail.com <duyfaker04@gmail.com>
2022-09-15 08:45:26 +0700

    First Commt

---
+++ models/xlm_roberta.py
@@ -0,0 +1,146 @@
+from __future__ import absolute_import, division, print_function
+
+import logging
+
+import torch
+from torch import nn
+from torchcrf import CRF
+from transformers import (XLMRobertaForTokenClassification)
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class PuncXLMRModel(XLMRobertaForTokenClassification):
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, valid_ids=None,
+                attention_mask_label=None):
+        sequence_output = \
+            self.roberta(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, head_mask=None)[0]
+        batch_size, max_len, feat_dim = sequence_output.shape
+        valid_output = torch.zeros(batch_size, max_len, feat_dim, dtype=torch.float32, device='cuda')
+        for i in range(batch_size):
+            jj = -1
+            for j in range(max_len):
+                if valid_ids[i][j].item() == 1:
+                    jj += 1
+                    valid_output[i][jj] = sequence_output[i][j]
+        sequence_output = self.dropout(valid_output)
+        logits = self.classifier(sequence_output)
+
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss(ignore_index=0)
+            # Only keep active parts of the loss
+            # attention_mask_label = None
+            if attention_mask_label is not None:
+                active_loss = attention_mask_label.view(-1) == 1
+                active_logits = logits.view(-1, self.config.num_labels)[active_loss]
+                active_labels = labels.view(-1)[active_loss]
+                loss = loss_fct(active_logits, active_labels)
+            else:
+                loss = loss_fct(logits.view(-1, self.config.num_labels), labels.view(-1))
+            return loss
+        else:
+            return logits
+
+
+class PuncXLMRCrfModel(XLMRobertaForTokenClassification):
+    def __init__(self, config):
+        super(PuncXLMRCrfModel, self).__init__(config=config)
+        self.crf = CRF(config.num_labels, batch_first=True)
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, valid_ids=None,
+                attention_mask_label=None):
+        sequence_output = \
+        self.roberta(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, head_mask=None)[0]
+        batch_size, max_len, feat_dim = sequence_output.shape
+        valid_output = torch.zeros(batch_size, max_len, feat_dim, dtype=torch.float32, device='cuda')
+        for i in range(batch_size):
+            jj = -1
+            for j in range(max_len):
+                if valid_ids[i][j].item() == 1:
+                    jj += 1
+                    valid_output[i][jj] = sequence_output[i][j]
+        sequence_output = self.dropout(valid_output)
+        logits = self.classifier(sequence_output)
+
+        if labels is not None:
+            log_likelihood = self.crf(logits, labels, mask=attention_mask_label.type(torch.uint8))
+            return -1.0 * log_likelihood
+        else:
+            sequence_tags = self.crf.decode(logits)
+            return sequence_tags
+
+
+class PuncXLMRLstmModel(XLMRobertaForTokenClassification):
+    def __init__(self, config):
+        super(PuncXLMRLstmModel, self).__init__(config=config)
+        self.lstm = nn.LSTM(input_size=config.hidden_size, hidden_size=config.hidden_size // 2, num_layers=2,
+                            batch_first=True, bidirectional=True)
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, valid_ids=None,
+                attention_mask_label=None):
+        sequence_output = \
+            self.roberta(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, head_mask=None)[0]
+
+        batch_size, max_len, feat_dim = sequence_output.shape
+        valid_output = torch.zeros(batch_size, max_len, feat_dim, dtype=torch.float32, device='cuda')
+        for i in range(batch_size):
+            jj = -1
+            for j in range(max_len):
+                if valid_ids[i][j].item() == 1:
+                    jj += 1
+                    valid_output[i][jj] = sequence_output[i][j]
+
+        sequence_output, _ = self.lstm(valid_output)
+        sequence_output = self.dropout(sequence_output)
+        logits = self.classifier(sequence_output)
+
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss(ignore_index=0)
+            # Only keep active parts of the loss
+            # attention_mask_label = None
+            if attention_mask_label is not None:
+                active_loss = attention_mask_label.view(-1) == 1
+                active_logits = logits.view(-1, self.config.num_labels)[active_loss]
+                active_labels = labels.view(-1)[active_loss]
+                loss = loss_fct(active_logits, active_labels)
+            else:
+                loss = loss_fct(logits.view(-1, self.config.num_labels), labels.view(-1))
+            return loss
+        else:
+            return logits
+
+
+class PuncXLMRLstmCrfModel(XLMRobertaForTokenClassification):
+    def __init__(self, config):
+        super(PuncXLMRLstmCrfModel, self).__init__(config=config)
+        self.crf = CRF(config.num_labels, batch_first=True)
+        self.lstm = nn.LSTM(input_size=config.hidden_size, hidden_size=config.hidden_size // 2, num_layers=2,
+                            batch_first=True, bidirectional=True)
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, valid_ids=None,
+                attention_mask_label=None):
+        sequence_output = \
+            self.roberta(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, head_mask=None)[0]
+        sequence_output, _ = self.lstm(sequence_output)
+
+        batch_size, max_len, feat_dim = sequence_output.shape
+        valid_output = torch.zeros(batch_size, max_len, feat_dim, dtype=torch.float32, device='cuda')
+        for i in range(batch_size):
+            jj = -1
+            for j in range(max_len):
+                if valid_ids[i][j].item() == 1:
+                    jj += 1
+                    valid_output[i][jj] = sequence_output[i][j]
+        sequence_output = self.dropout(valid_output)
+        logits = self.classifier(sequence_output)
+
+        if labels is not None:
+            log_likelihood = self.crf(logits, labels, mask=attention_mask_label.type(torch.uint8))
+            return -1.0 * log_likelihood
+        else:
+            sequence_tags = self.crf.decode(logits)
+            return sequence_tags
